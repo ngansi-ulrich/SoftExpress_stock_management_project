ebc3a186c2c304ec1c44f3ce95c79f1abf6dd8ec
plug <plug@localhost>
2026-08-12 23:00:37 +0100

    Fix agency routing and update agency module views and templates

--- employees/forms.py
+++ employees/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from accounts.models import Employee
+from agencies.models import Agency
+
 
 class EmployeeForm(forms.ModelForm):
 
@@ -14,3 +16,34 @@ class EmployeeForm(forms.ModelForm):
             'role',
             'agency'
         ]
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: Jean'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: Mballa'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'employee@softexpress.com'
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': '+237 6xx xxx xxx'
+            }),
+            'role': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+            'agency': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # only let active agencies be assigned
+        self.fields['agency'].queryset = Agency.objects.filter(is_active=True)
+        self.fields['agency'].empty_label = "Select agency"
